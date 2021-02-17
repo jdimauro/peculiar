393c2102e85b56943c5a674ca9214642715a0bb5
jdimauro <jazz@witchlights.com>
2021-02-17 15:19:37 -0500

    getting set up and testing IDE with some deliberate bugs

--- arduino/peculiar-fastled/peculiar-fastled.ino
+++ arduino/peculiar-fastled/peculiar-fastled.ino
@@ -1,4 +1,21 @@
 // The Witch's Peculiar
+// Being a Witch Lights sprite animation that plays on a shorter, denser pixel strip as part of a wearable garment.
+
+#include <FastLED.h>
+#include <config.h>
+
+
+#ifndef PSTR
+#define PSTR // Make Arduino Due happy
+#endif
+
+#define NUM_LEDS							300 // 300 per 5-meter strip at 60 pixels/meter 
+#define NEOPIXEL_DATA_PIN			6								// Pin for neopixels
+
+// CRGB leds[NUM_LEDS];
+
+// Function prototypes:
+void resetStrip(void);
 
 
 
@@ -11,3 +28,16 @@ void loop()
 {
 	
 }
+
+void resetStrip() 
+{
+		FastLED.addLeds<NEOPIXEL, NEOPIXEL_DATA_PIN>(leds, NUM_LEDS);
+
+		for (int i = 0; i < NUM_LEDS; i++) {
+			leds[i] = CRGB::Black;
+		}
+
+		FastLED.show();
+}
+
+terriblesyntaxerror
